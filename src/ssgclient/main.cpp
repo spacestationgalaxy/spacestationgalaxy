881d1deb407bdcc5506ceba14f7a6980f9269709
Nicholas Keane <nickkeane23@gmail.com>
2017-01-04 21:38:19 -0500

    CMake Layout

--- src/ssgclient/main.cpp
+++ src/ssgclient/main.cpp
@@ -1,7 +1,24 @@
 //
 // Created by Nick on 1/4/2017.
 //
+
+#include <iostream>
+
+using namespace std;
+
+const char* gszTitle[32] = {
+        "==============================",
+        "== SpaceStationGalaxy  v1.0 ==",
+        "==============================",
+        "====        CLIENT        ====",
+        "=============================="
+};
+
 int main(int argc, char** argv)
 {
+    for (int i = 0; i < 5; ++i)
+        cout << gszTitle[i] << endl;
+    cout << endl;
+
     return 0;
 }
